cc0aac256d4092e889ed83d7572fdaf3c04cc856
41591527 <41591527@qq.com>
2022-08-02 21:56:15 +0800

    乘客api，发送验证码服务完成。

--- api-massenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-massenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -5,7 +5,10 @@ import com.mashibing.dto.ResponseResult;
 import com.mashibing.response.NumberCodeResponse;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Auther: tutu
@@ -19,22 +22,30 @@ public class VerificationCodeService {
     @Autowired
     private ServiceVerificationClient serviceVerificationClient;
 
-    public String generatorCode(String passengerPhone){
+    //乘客验证码的前缀
+    private String verificationCodePrefix = "passenger-verification-code-";
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public ResponseResult generatorCode(String passengerPhone){
         //调用验证码服务，获取验证码
-        System.out.println("调用验证码服务，获取验证码");
         ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationClient.getNumberCode(6);
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
         System.out.println("remote number code: " + numberCode);
 
+
         //存入redis
-        System.out.println("存入redis");
 
-        //返回值
-        JSONObject result = new JSONObject();
-        result.put("code",1);
-        result.put("message","success");
+        //key,value,过期时间
+        String key = verificationCodePrefix + passengerPhone;
+        //存入redis
+        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
 
-        return result.toString();
+        //通过短信服务商，将对应的验证码发送到手机上。
+
+
+        return ResponseResult.success("");
     }
 }
