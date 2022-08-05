b184eb8544ddf21c25c11006dedd5a7df51273a2
41591527 <41591527@qq.com>
2022-08-05 21:50:49 +0800

    根据手机号查询用户信息

---
+++ service-passenger-user/src/main/java/com/mashibing/dto/PassengerUser.java
@@ -0,0 +1,30 @@
+package com.mashibing.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Auther: tutu
+ * @Date: 2022/8/5 - 08 - 05 - 21:13
+ * @Description: com.mashibing.dto
+ * @version: 1.0
+ */
+
+@Data
+public class PassengerUser {
+
+    private Long id;
+
+    private LocalDateTime gmtCreate;
+
+    private LocalDateTime gmtModified;
+
+    private String passengerPhone;
+
+    private String passengerName;
+
+    private byte passengerGender;
+
+    private byte state;
+}
---
+++ service-passenger-user/src/main/java/com/mashibing/mapper/PassengerUserMapper.java
@@ -0,0 +1,15 @@
+package com.mashibing.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mashibing.dto.PassengerUser;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Auther: tutu
+ * @Date: 2022/8/5 - 08 - 05 - 21:18
+ * @Description: com.mashibing.mapper
+ * @version: 1.0
+ */
+@Repository
+public interface PassengerUserMapper extends BaseMapper<PassengerUser> {
+}
