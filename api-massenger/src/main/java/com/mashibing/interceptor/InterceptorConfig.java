27f61374dc80fd5dad8d8e8f2678a8ec40904602
41591527 <41591527@qq.com>
2022-08-09 21:06:40 +0800

    token拦截器测试

---
+++ api-massenger/src/main/java/com/mashibing/interceptor/InterceptorConfig.java
@@ -0,0 +1,24 @@
+package com.mashibing.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Auther: tutu
+ * @Date: 2022/8/9 - 08 - 09 - 20:53
+ * @Description: com.mashibing.interceptor
+ * @version: 1.0
+ */
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JWTInterceptor())
+                //拦截的路径
+                .addPathPatterns("/**")
+                 //不拦截的路径
+                .excludePathPatterns("/noauthTest");
+    }
+}
